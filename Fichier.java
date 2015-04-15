b7a944ed31de70ef0d0816820ca6e67a7e88926b
Jeremie <jeremie.decome12@gmail.com>
2015-04-15 22:55:36 +0200

    Classe Fichier terminé

--- Fichier.java
+++ Fichier.java
@@ -1,6 +1,6 @@
 /* Fichier Fichier.java
 Crée le dimanche 12 avril 2015
-MAJ : dimanche 12 avril 2015
+MAJ : mercredi 15 avril 2015
 Description : Gère l'interaction avec le fichier XML présent sur le disque */
 
 import java.io.File;
@@ -25,30 +25,20 @@ import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import java.lang.Enum;
+import org.xml.sax.SAXException;
+import java.io.IOException;
 
 public class Fichier
 {
 	private File fichier;
 	private String dossier = "saves/";
-	private String[] data;
+	private String[] data;		// est l'image du fichier XML au chargement et sert de tampon
 	private String[] etats;
+	//private String[] defaut;	// n'a pas l'air d'etre utile en attribut
 	private int nb = 13;
 	private Element[] elements;
 	private Element racine;
 	private Element tama;
-	private Element nom;
-	private Element type;
-	private Element vie;
-	private Element faim;
-	private Element fatigue;
-	private Element energie;
-	private Element moral;
-	private Element sante;
-	private Element toilettes;
-	private Element maison;
-	private Element dormir;
-	private Element deplacement;
-	private Element lastMAJ;
 	private String nomFichier;
 	public Fichier(String nomTama, String typeTama)
 	{
@@ -58,111 +48,20 @@ public class Fichier
 		this.etats = new String[] {"nom", "type", "vie", "faim", "fatigue", "energie", "moral", "sante", "toilettes", "maison", "dormir", "deplacement", "lastMAJ"};
 		// On construit le nom du fichier
 		this.nomFichier = this.dossier + nomTama + "-" + typeTama + ".xml";
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		try
+		if(this.isFileExisted(this.nomFichier))
 		{
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document= builder.newDocument();
-			this.racine = document.createElement("application");
-			document.appendChild(racine);
-
-			this.tama = document.createElement("tamagotchi");
-			racine.appendChild(tama);
-
-			this.nom = document.createElement("nom");
-			this.nom.appendChild(document.createTextNode(nomTama));
-			
-			this.type = document.createElement("type");
-			this.type.appendChild(document.createTextNode(typeTama));
-
-			this.vie = document.createElement("vie");
-			this.vie.appendChild(document.createTextNode("100"));
-
-			this.faim = document.createElement("faim");
-			this.faim.appendChild(document.createTextNode("0"));
-
-			this.fatigue = document.createElement("fatigue");
-			this.fatigue.appendChild(document.createTextNode("0"));
-
-			this.energie = document.createElement("energie");
-			this.energie.appendChild(document.createTextNode("0"));
-
-			this.moral = document.createElement("moral");
-			this.moral.appendChild(document.createTextNode("90"));
-
-			this.sante = document.createElement("sante");
-			this.sante.appendChild(document.createTextNode("100"));
-
-			this.toilettes = document.createElement("toilettes");
-			this.toilettes.appendChild(document.createTextNode("0"));
-
-			this.maison = document.createElement("maison");
-			this.maison.appendChild(document.createTextNode("false"));
-
-			this.dormir = document.createElement("dormir");
-			this.dormir.appendChild(document.createTextNode("false"));
-
-			this.deplacement = document.createElement("deplacement");
-			this.deplacement.appendChild(document.createTextNode("false"));
-
-			long timestamp = System.currentTimeMillis() / 1000;
-			this.lastMAJ = document.createElement("lastMAJ");
-			this.lastMAJ.appendChild(document.createTextNode(Long.toString(timestamp)));
-				
-			this.tama.appendChild(this.nom);
-			this.tama.appendChild(this.type);
-			this.tama.appendChild(this.vie);
-			this.tama.appendChild(this.faim);
-			this.tama.appendChild(this.fatigue);
-			this.tama.appendChild(this.energie);
-			this.tama.appendChild(this.moral);
-			this.tama.appendChild(this.sante);
-			this.tama.appendChild(this.toilettes);
-			this.tama.appendChild(this.maison);
-			this.tama.appendChild(this.dormir);
-			this.tama.appendChild(this.deplacement);
-			this.tama.appendChild(this.lastMAJ);
-
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(document);
-			this.fichier = new File(nomFichier);
-			StreamResult sortie = new StreamResult(this.fichier);
-			
-			//prologue
-			transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
-			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-			transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");			
-	    		
-			//formatage
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-			
-			//sortie
-			transformer.transform(source, sortie);
-			this.elements[0] = this.nom;
-			this.elements[1] = this.type;
-			this.elements[2] = this.vie;
-			this.elements[3] = this.faim;
-			this.elements[4] = this.fatigue;
-			this.elements[5] = this.energie;
-			this.elements[6] = this.moral;
-			this.elements[7] = this.sante;
-			this.elements[8] = this.toilettes;
-			this.elements[9] = this.maison;
-			this.elements[10] = this.dormir;
-			this.elements[11] = this.deplacement;
-			this.elements[12] = this.lastMAJ;
+			System.out.println("Erreur, le fichier existe déjà !");
+			System.exit(-1);
 		}
-		catch(Exception e)
-		{
-    		e.printStackTrace();
-		}
+		this.ecrireXML(true, nomTama, typeTama);
 	}
 	public Fichier(String nomFichier)
 	{
-		System.out.println("Ouvre le fichier " + dossier + nomFichier);
-		// Mettre une sécurité si le fichier n'existe pas
+		if(!this.isFileExisted(this.dossier + nomFichier)) // Mettre une sécurité si le fichier n'existe pas, on stoppe le chargement
+		{
+			System.out.println("Erreur, le fichier " + nomFichier + " n'existe pas !");
+			System.exit(-1);
+		}
 		this.data = new String[this.nb];
 		this.elements = new Element[this.nb];
 		this.etats = new String[] {"nom", "type", "vie", "faim", "fatigue", "energie", "moral", "sante", "toilettes", "maison", "dormir", "deplacement", "lastMAJ"};
@@ -184,7 +83,7 @@ public class Fichier
 					for(int j = 0; j < this.nb; j++)
 					{
 						this.elements[j] = (Element) this.tama.getElementsByTagName(this.etats[j]).item(0);
-						//System.out.println("Etat XML n°" + j + " => " + this.elements[j].getTextContent());
+						this.data[j] = this.elements[j].getTextContent();
 					}
 				}
 			}
@@ -194,26 +93,75 @@ public class Fichier
 			e.printStackTrace();
 		}
 	}
-	private void stockageTableau()
+	private void ecrireXML(boolean nouveau, String nomTama, String typeTama)
 	{
-		
+		try
+		{
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.newDocument();
+			this.racine = document.createElement("application");
+			document.appendChild(racine);
+			this.tama = document.createElement("tamagotchi");
+			racine.appendChild(this.tama);
+			if(nouveau)		// ajout des infos dans un nouveau fichier
+			{
+				long timestamp = System.currentTimeMillis() / 1000;
+				String[] defaut = new String[] {nomTama, typeTama, "100", "0", "0", "0", "90", "100", "0", "false", "false", "false", Long.toString(timestamp)};
+				this.racine = document.createElement("application");
+				for(int i = 0; i < this.nb; i++)
+				{
+					this.elements[i] = document.createElement(this.etats[i]);
+					this.elements[i].appendChild(document.createTextNode(defaut[i]));
+					this.tama.appendChild(this.elements[i]);
+					this.data[i] = defaut[i];
+				}	
+			}
+			else 			// modification des infos dans le fichier (donc sauvegarde)
+			{
+				for(int i = 0; i < this.nb; i++)
+				{
+					this.elements[i] = document.createElement(this.etats[i]);
+					this.elements[i].appendChild(document.createTextNode(this.data[i]));
+					this.tama.appendChild(this.elements[i]);
+				}
+			}
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			if(nouveau)
+				this.fichier = new File(nomFichier);
+			StreamResult sortie = new StreamResult(this.fichier);
+			//prologue
+			transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+			//formatage
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			transformer.transform(source, sortie);	// sortie vers le fichier
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
-	public String getElement(int i)
+	private boolean isFileExisted(String nom)
 	{
-		return this.elements[i].getTextContent();
+		File f = new File(nom);
+		if(f.exists() && f.isFile())
+			return true;
+		else
+			return false;
 	}
+	
 	public boolean majEtat(Etat etat, int valeur)
 	{
 		int pos = etat.ordinal();
 		if((pos >= 2) && (pos <= 8))	// cases du tableau correspondant aux états
 		{
-			if(this.elements[pos].hasAttribute(this.etats[pos]))
-			{
-				this.elements[pos].setAttribute(this.etats[pos], Integer.toString(valeur));
-				return true;
-			}
-			else
-				return false;
+			this.data[pos] = Integer.toString(valeur);
+			return true;
 		}
 		else
 			return false;
@@ -221,57 +169,37 @@ public class Fichier
 	public boolean majEtat(Etat etat, boolean valeur)
 	{
 		int pos = etat.ordinal();
-		String val;
 		if((pos >= 9) && (pos <= 11))	// cases du tableau correspondant aux états
 		{
 			if(valeur)
-				val = "true";
+				this.data[pos] = "true";
 			else
-				val = "false";
-			this.elements[pos].setAttribute(this.etats[pos], val);
+				this.data[pos] = "false";
 			return true;
 		}
 		else
 			return false;
 	}
+	
+	// temporaire
+	public void afficheElement(int pos)
+	{
+		System.out.println(pos + ") Etat " + this.etats[pos] + " : " + this.data[pos]);
+	}
+	
+	public void sauvegarde()
+	{
+		for(int i = 0; i < this.nb; i++)
+		{
+			this.tama.setAttribute(this.etats[i], this.data[i]);
+		}
+		this.ecrireXML(false, null, null);
+		
+	}
+	// Getteurs
 	public String getNom()
 	{
 		return this.nomFichier;
 	}
-	public void afficheElement(int pos)
-	{
-		System.out.println(pos + ") Etat " + this.etats[pos] + " : " + this.getElement(pos));
-	}
-	public void sauvegarde()
-	{
-		System.out.println("Ecriture des informations dans le fichier, appelé à la fermeture de l'application");
-		/*for(int i = 0; i < this.nb; i++)
-		{
-			System.out.println("Etat " + this.etats[i] + " : " + this.getElement(i));
-		}//*/
-	}
-	public static void main(String[] args)
-	{
-		Etat etat;
-		System.out.println("Essai de la classe Fichier");
-		Fichier fichier = new Fichier("Robert-Animal.xml");
-		System.out.println("Juste après l'ouverture");
-		for(int i = 0; i < 13; i++)
-			fichier.afficheElement(i);
-		System.out.println("--------------------");
-		if(fichier.majEtat(Etat.VIE, 98))
-		{
-			System.out.println("Modification du point de vie");
-			for(int i = 0; i < 13; i++)
-				fichier.afficheElement(i);
-		}
-		System.out.println("--------------------");
-		if(fichier.majEtat(Etat.DORMIR, true))
-		{
-			System.out.println("Modification de dormir");
-			for(int i = 0; i < 13; i++)
-				fichier.afficheElement(i);
-		}
-		fichier.sauvegarde();
-	}
+	// Setteurs
 }
